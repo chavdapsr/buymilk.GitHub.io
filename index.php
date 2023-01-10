cade9c81562f5f01fbfe628e5f30beb8360f989d
chavdapsr <47287764+chavdapsr@users.noreply.github.com>
2023-01-10 19:51:07 +0530

    Update index.php
    Signed-off-by: chavdapsr <47287764+chavdapsr@users.noreply.github.com>

--- index.php
+++ index.php
@@ -6,24 +6,12 @@
    <?php
       require('navbar.php');
    ?>
-  
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" http-equiv="Content-Type" content="width=device-width,initial-scale=1.0">
-       <title>index</title>
-      </head>
-    <body>
-    <?php 
-    require('home.php');
-     ?>    
+
+    
 <?php 
     require('footer.php');
 ?>
 
 
-    </body>
     
-</html>
+    
